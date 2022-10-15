b8bafa4b929ab78b6374d4eea1f7e3d2be57cf21
Mateusz Pryl <242228@edu.p.lodz.pl>
2022-10-15 14:31:57 +0200

    Testing commit pt.2

---
+++ labs/project/OOP/library/src/functions.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+int function() {
+    cout << "Hello, Great Scoooott!" << endl;
+    return 1;
+}
---
+++ labs/project/OOP/program/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "functions.h"
+#include "Student.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello, Woddddddrld!" << endl;
+    function();
+
+    Student student("Adaaam", "Smith");
+    return 0;
+}
