567c19291eb339e4f0d50cdb3d0473dff5ab04a4
Mateusz <mateusz.pryl@gmail.com>
2022-11-14 14:50:46 +0100

    Added extra validation to ContainerStack functions

--- labs/project/OOP/library/src/ContainerStack.cpp
+++ labs/project/OOP/library/src/ContainerStack.cpp
@@ -17,13 +17,28 @@ int ContainerStack::getSize() { //How many containers are on the stack
 }
 
 Container& ContainerStack::at(int height) {
-    return containers[height-1];
+    if (height > getSize()) {
+        Container containerSkeleton;
+        return containerSkeleton;
+    } else {
+        return containers[height-1];
+    }
 }
 
 void ContainerStack::take(Container aContainer) {
-    containers.push_back(aContainer);
+    int capacity = getCapacity();
+    std::cout << capacity;
+    if (capacity == 0) {
+        std::cout << "Can not load more containers" << std::endl;
+    } else {
+        containers.push_back(aContainer);
+    }
 }
 
-Container ContainerStack::give(){
-    containers.pop_back();
+Container ContainerStack::give() {
+    if (getSize() == 0) {
+        std::cout << "Can't remove a container from an empty stack";
+    } else {
+        containers.pop_back();
+    }
 }
