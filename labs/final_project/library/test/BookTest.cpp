eef3470c5c80d3b0d1fa873bb760034196c2fc60
Mateusz <mateusz.pryl@gmail.com>
2023-01-06 16:56:13 +0100

    CMakeLists.txt config + test environment

---
+++ labs/final_project/library/test/BookTest.cpp
@@ -0,0 +1,11 @@
+#include <boost/test/unit_test.hpp>
+
+
+
+BOOST_AUTO_TEST_SUITE(BookTestSuite)
+
+    BOOST_AUTO_TEST_CASE(Crane_is_Parked) {
+        BOOST_CHECK_EQUAL(1, 1);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
